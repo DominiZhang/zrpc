23541812bc117169c82331c0d2204e65ff18fc16
DomiZhang <ttmfyyue@qq.com>
2023-01-21 00:10:10 +0800

    完善注册服务以及spring创建bean时相关操作完善

--- src/main/java/com/zzd/zrpc/provider/impl/ZkServiceProviderImpl.java
+++ src/main/java/com/zzd/zrpc/provider/impl/ZkServiceProviderImpl.java
@@ -1,25 +1,71 @@
 package com.zzd.zrpc.provider.impl;
 
 import com.zzd.zrpc.config.RpcServiceConfig;
+import com.zzd.zrpc.enums.RpcErrorMessageEnum;
+import com.zzd.zrpc.exception.RpcException;
+import com.zzd.zrpc.extension.ExtensionLoader;
 import com.zzd.zrpc.provider.ServiceProvider;
+import com.zzd.zrpc.registry.ServiceRegistry;
+import com.zzd.zrpc.remoting.transport.netty.server.NettyRpcServer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author zzd
  * @date 2023/1/12
  */
+@Slf4j
 public class ZkServiceProviderImpl implements ServiceProvider {
+
+    /**
+     * key: rpc service name(interface name + version + group)
+     * value: service object
+     */
+    private final Map<String, Object> serviceMap;
+    private final Set<String> registeredService;
+    private final ServiceRegistry serviceRegistry;
+
+    public ZkServiceProviderImpl() {
+        serviceMap = new ConcurrentHashMap<>();
+        // 记录已经注册的服务类的名称
+        registeredService = ConcurrentHashMap.newKeySet();
+        serviceRegistry = ExtensionLoader.getExtensionLoader(ServiceRegistry.class).getExtension("zk");
+    }
+
     @Override
     public void addService(RpcServiceConfig rpcServiceConfig) {
-
+        String rpcServiceName = rpcServiceConfig.getRpcServiceName();
+        if (registeredService.contains(rpcServiceName)) {
+            return;
+        }
+        registeredService.add(rpcServiceName);
+        serviceMap.put(rpcServiceName, rpcServiceConfig.getService());
+        log.info("Add service: {} and interfaces:{}", rpcServiceName, rpcServiceConfig.getService().getClass().getInterfaces());
     }
 
     @Override
     public Object getService(String rpcServiceName) {
-        return null;
+        Object service = serviceMap.get(rpcServiceName);
+        if (null == service) {
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND);
+        }
+        return service;
     }
 
     @Override
     public void publishService(RpcServiceConfig rpcServiceConfig) {
-
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            this.addService(rpcServiceConfig);
+            serviceRegistry.registerService(rpcServiceConfig.getRpcServiceName(), new InetSocketAddress(host, NettyRpcServer.PORT));
+        } catch (UnknownHostException e) {
+            log.error("occur exception when getHostAddress", e);
+        }
     }
 }
